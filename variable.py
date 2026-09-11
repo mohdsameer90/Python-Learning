9b6cdb82d90bca7e838985b995e7fe37510f4e6b
Mohd Sameer <324277840+mohdsameer90@users.noreply.github.com>
2026-09-11 07:10:56 +0530

    Add variable.py

---
+++ variable.py
@@ -0,0 +1,12 @@
+name = "Mohd Sameeer"
+age = 19
+
+print(name)
+print(age)
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplivation:", a * b)
+print("Division:", a / b)
